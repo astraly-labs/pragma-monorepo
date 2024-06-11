a2d29ab2a256b27d1049f73ca3740c2dbf1ad98e
0xevolve <Artevolve@yahoo.com>
2024-06-11 14:11:17 +0100

    fix: solidity ci

---
+++ solidity/test/Pragma.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Pragma} from "../src/Pragma.sol";
+
+contract PragmaT is Test {
+    Pragma public pragma_;
+
+    function setUp() public {
+        pragma_ = new Pragma();
+    }
+}
