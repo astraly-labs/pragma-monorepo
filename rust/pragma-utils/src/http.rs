ba04e53332f42fcedacbc66f9247035c632f3e5c
akhercha <akherchache@pm.me>
2024-09-05 23:57:24 +0200

    feat(hyperlane_announcements): Source

--- rust/pragma-utils/src/http.rs
+++ rust/pragma-utils/src/http.rs
@@ -1,3 +1,5 @@
+// Source:
+// https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/3e90734310fb1ca9a607ce3d334015fa7aaa9208/rust/hyperlane-base/src/types/utils.rs#L10
 use std::time::Duration;
 
 use anyhow::Result;
