7efd9879b02a73e3ff2269f260b051cd2c874b2b
akhercha <akherchache@pm.me>
2024-09-05 21:51:18 +0200

    feat(hyperlane_announcements): Clean stuff

--- rust/theoros/src/types/hyperlane/mod.rs
+++ rust/theoros/src/types/hyperlane/mod.rs
@@ -1,12 +1,10 @@
 pub mod checkpoints;
 pub mod dispatch_event;
 pub mod settings;
-pub mod signature;
 pub mod storages;
 
 pub use checkpoints::*;
 pub use dispatch_event::*;
 pub use settings::*;
 #[allow(unused)]
-pub use signature::*;
 pub use storages::*;
