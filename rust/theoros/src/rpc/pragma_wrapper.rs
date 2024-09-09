f67dfe234c570f82d06889b489892a335d8e9b53
akhercha <akherchache@pm.me>
2024-09-09 16:29:11 +0200

    feat(theoros_validators): Docs'

--- rust/theoros/src/rpc/pragma_wrapper.rs
+++ rust/theoros/src/rpc/pragma_wrapper.rs
@@ -3,6 +3,6 @@ use starknet::core::types::Felt;
 #[allow(unused)]
 #[async_trait::async_trait]
 pub trait PragmaWrapperCalls {
-    // TODO: Retrieve all data feeds
+    /// Retrieves all the available data feeds from the Pragma oracle Wrapper.
     async fn get_data_feeds(&self, pragma_wrapper_address: &Felt) -> anyhow::Result<Vec<String>>;
 }
