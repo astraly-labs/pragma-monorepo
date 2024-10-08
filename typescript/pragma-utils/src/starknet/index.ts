75b4edea494e7c79917911378bf87b9400167df6
akhercha <akherchache@pm.me>
2024-10-08 14:06:34 +0200

    feat(scripts): Rpcs re-export

--- typescript/pragma-utils/src/starknet/index.ts
+++ typescript/pragma-utils/src/starknet/index.ts
@@ -1,8 +1,8 @@
 // Adapted from Vesu's typescript code:
 // https://github.com/vesuxyz/vesu-liquidate/tree/main/lib
 
-export * from "./logs";
 export * from "./deployer";
 export * from "./gas";
 export * from "./receipts";
 export * from "./setup";
+export * from "./rpcs";
