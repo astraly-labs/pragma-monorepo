6691916957d7e9088860e0651243ee9ad4285bb2
devops72-sre <>
2024-10-26 23:42:33 +0530

    Added ci

---
+++ infra/theores/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=${INFISICAL_CLIENT_ID} --client-secret=${INFISICAL_CLIENT_SECRET} --silent --plain)
+infisical export  --projectId=${INFISICAL_PROJECT_ID} --env=${INFISICAL_ENV}  --path=${INFISICAL_APP_PATH} > .env
+source .env
+exec theoros ${ARGS}
