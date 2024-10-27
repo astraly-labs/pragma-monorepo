98d484146540508897638f1238d54f7e93e32637
0xevolve <Artevolve@yahoo.com>
2024-10-27 16:05:41 +0000

    feat: structure ws route

--- rust/theoros/src/handlers/websocket/subscribe_to_calldata.rs
+++ rust/theoros/src/handlers/websocket/subscribe_to_calldata.rs
@@ -1,1 +1,77 @@
-// TODO: Websocket endpoint for which we subscribe to calldata.
+use {
+    crate::{types::pragma::constants::MAX_CLIENT_MESSAGE_SIZE, AppState},
+    anyhow::{anyhow, Result},
+    axum::{
+        extract::{
+            ws::{Message, WebSocket, WebSocketUpgrade},
+            State as AxumState,
+        },
+        http::HeaderMap,
+        response::IntoResponse,
+    },
+    futures::{
+        stream::{SplitSink, SplitStream},
+        SinkExt, StreamExt,
+    },
+    prometheus_client::{
+        encoding::{EncodeLabelSet, EncodeLabelValue},
+        metrics::{counter::Counter, family::Family},
+    },
+    serde::{Deserialize, Serialize},
+    std::{
+        collections::HashMap,
+        net::IpAddr,
+        num::NonZeroU32,
+        sync::{
+            atomic::{AtomicUsize, Ordering},
+            Arc,
+        },
+    },
+    tokio::sync::{broadcast::Receiver, watch},
+};
+
+#[derive(Clone)]
+pub struct PriceFeedClientConfig {
+    verbose: bool,
+    binary: bool,
+    allow_out_of_order: bool,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+pub enum Interaction {
+    NewConnection,
+    CloseConnection,
+    ClientHeartbeat,
+    PriceUpdate,
+    ClientMessage,
+    RateLimit,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+pub enum Status {
+    Success,
+    Error,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, EncodeLabelSet)]
+pub struct Labels {
+    pub interaction: Interaction,
+    pub status: Status,
+}
+
+pub struct WsMetrics {
+    pub interactions: Family<Labels, Counter>,
+}
+
+pub fn ws_route_handler(
+    ws: WebSocketUpgrade,
+    AxumState(state): AxumState<AppState>,
+    headers: HeaderMap,
+) -> impl IntoResponse {
+    ws.max_message_size(MAX_CLIENT_MESSAGE_SIZE).on_upgrade(move |socket| websocket_handler(socket, state))
+}
+
+#[tracing::instrument(skip(stream, state))]
+async fn websocket_handler(stream: WebSocket, state: AppState) {
+    todo!();
+}
--- rust/theoros/src/types/pragma/constants.rs
+++ rust/theoros/src/types/pragma/constants.rs
@@ -1,1 +1,10 @@
+use std::time::Duration;
+
 pub const HYPERLANE_VERSION: u8 = 3;
+
+pub const PING_INTERVAL_DURATION: Duration = Duration::from_secs(30);
+pub const MAX_CLIENT_MESSAGE_SIZE: usize = 100 * 1024; // 100 KiB
+
+/// The maximum number of bytes that can be sent per second per IP address.
+/// If the limit is exceeded, the connection is closed.
+pub const BYTES_LIMIT_PER_IP_PER_SECOND: u32 = 256 * 1024; // 256 KiB
