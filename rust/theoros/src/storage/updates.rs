733e8dfd4a95badadc6bee792b52d906d6b49981
akhercha <akherchache@pm.me>
2024-11-01 15:23:48 +0100

    dev(better_theoros): Simpler struct

--- rust/theoros/src/storage/updates.rs
+++ rust/theoros/src/storage/updates.rs
@@ -8,21 +8,19 @@ use crate::types::hyperlane::DispatchUpdateInfos;
 
 /// Contains a mapping between feed ids and their latest dispatch update.
 #[derive(Debug, Default)]
-pub struct LatestUpdatePerFeedStorage {
-    events: RwLock<HashMap<U256, DispatchUpdateInfos>>,
-}
+pub struct LatestUpdatePerFeedStorage(RwLock<HashMap<U256, DispatchUpdateInfos>>);
 
 impl LatestUpdatePerFeedStorage {
     /// Insert the latest [`DispatchUpdateInfos`] for a feed id.
     pub async fn add(&self, feed_id: U256, event: DispatchUpdateInfos) -> Result<()> {
-        let mut events = self.events.write().await;
+        let mut events = self.0.write().await;
         events.insert(feed_id, event);
         Ok(())
     }
 
     /// Retrieves the latest [`DispatchUpdateInfos`] for a feed id.
     pub async fn get(&self, feed_id: &U256) -> Result<Option<DispatchUpdateInfos>> {
-        let events = self.events.read().await;
+        let events = self.0.read().await;
         Ok(events.get(feed_id).cloned())
     }
 }
